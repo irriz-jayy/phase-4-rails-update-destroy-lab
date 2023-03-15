c0283981a5898f083f993377bdb02cbe314b191b
irriz-jayy <jayfelix789@gmail.com>
2023-03-15 21:04:47 +0300

    Done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -19,8 +19,7 @@ class PlantsController < ApplicationController
   end
 
   def update
-    byebug
-    plant = Plant.find(params[:id])
+    plant =find_plant
     if plant
       plant.update(plant_params)
       render json: plant
@@ -29,8 +28,16 @@ class PlantsController < ApplicationController
     end
   end
 
-  private
+  def destroy
+    find_plant.destroy
+    head :no_content
+  end
 
+  private
+  def find_plant
+     Plant.find(params[:id])
+
+  end
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
